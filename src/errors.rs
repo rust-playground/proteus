da422a5dd82c9cca612c864a7d9905992bce8281
Dean Karn <Dean.Karn@gmail.com>
2020-05-25 13:37:48 -0700

    Minor updates (#3)

--- src/errors.rs
+++ src/errors.rs
@@ -1,27 +1,14 @@
 //! Errors that can occur applying transformations.
 
 use crate::actions::setter::Error as SetterError;
-use serde_json::Error as SerdeJSONError;
-use snafu::Snafu;
+use thiserror::Error;
 
 /// This type represents all possible errors that an occur while building and applying a Transformation.
-#[derive(Debug, Snafu)]
+#[derive(Error, Debug)]
 pub enum Error {
-    #[snafu(display("{}", err))]
-    Setter { err: SetterError },
+    #[error(transparent)]
+    Setter(#[from] SetterError),
 
-    #[snafu(display("{}", err))]
-    JSONError { err: SerdeJSONError },
-}
-
-impl From<SerdeJSONError> for Error {
-    fn from(err: SerdeJSONError) -> Self {
-        Error::JSONError { err }
-    }
-}
-
-impl From<SetterError> for Error {
-    fn from(err: SetterError) -> Self {
-        Error::Setter { err }
-    }
+    #[error(transparent)]
+    JSONError(#[from] serde_json::Error),
 }
